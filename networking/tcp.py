1366326596fcb239ac3adffe55ab18aa0b0e38e2
turnkeyz <kylertelge@gmail.com>
2022-03-12 16:59:28 -0500

    added first exercise and second parent director for networking modules

---
+++ networking/tcp.py
@@ -0,0 +1,23 @@
+import socket
+
+#establish our variables for who we want to test against
+target = "www.google.com"
+port = 80
+
+#setting up socket object named "client"
+##AF_INET specifies standard ipv4 connection
+##SOCK_STREAM specifies it will be a tcp connection
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connection to client via variables
+client.connect((target,port))
+
+#sending data to our client via http get request"
+client.send(b'get / HTTP/1.1\r\nHost: google.com\r\n\r\n')
+
+#specify response size
+response = client.recv(16240)
+
+#print response
+print(response.decode())
+client.close()
