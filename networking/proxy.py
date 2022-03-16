116a66ef6a3d7405c5992c4a150bfe298ac3df99
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-03-16 15:12:44 -0700

    finished up basic tcp proxy server and ready to start testing and debugging

--- networking/proxy.py
+++ networking/proxy.py
@@ -79,6 +79,55 @@ def proxy_handler(client_socket,remote_host,remote_port,receive_first):
             remote_socket.close()
             print("[*] No more data. Closing all connections.")
             break
-        
+
+def server_loop(local_host, local_port,
+                remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host,local_port))
+    except Exception as e:
+        print('Problem on binding to local: %r'% e)
+
+        print('[!!] Failed to listen on %s:%d'%(local_host,local_port))
+        print('[!!] Check for other listening sockets or correct permissions.')
+        sys.exit(3)
+    print('[*] Listening on %s:%d' % (local_host,local_port))
+    server.listen(10)
+    while True:
+        client_socket, addr = server.accept()
+        #print out local connection info
+        line = "> Received incoming connection from %s:%d" % (addr[0],addr[1])
+        print(line)
+        #start a thread to talk to remote host
+        proxy_thread = threading.Thread(
+            target=proxy_handler,
+            args = (client_socket, remote_host,
+            remote_port, receive_first))
+        proxy_thread.start()
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport]",end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(3)
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+
+    server_loop(local_host,local_port,
+                remote_host,remote_port,receive_first)
+
+if __name__ == '__main__':
+    main()
 
 hexdump('python rocks\n and proxies roll\n')
