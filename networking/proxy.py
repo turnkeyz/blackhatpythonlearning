f54fac516ebccc9be851badd3ad4a0e8db940376
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-03-16 14:54:42 -0700

    majority of the groundwork for a basic tcp proxy server has ben made

---
+++ networking/proxy.py
@@ -0,0 +1,84 @@
+import sys
+import socket
+import threading
+
+HEX_FILTER = ''.join(
+    [(len(repr(chr(1))) == 3) and chr(1) or '.'
+    for i in range(256)])
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src,bytes):
+        src = src.decode()
+    results = list()
+    for i in range(0,len(src),length):
+        word = str(src[i:i+length])
+        printable = word.translate(HEX_FILTER)
+        hexa = ''.join([f'{ord(c):02x}' for c in word])
+        hexwidth = length*3
+        results.append(f'{1:04x} {hexa:<{hexwidth}} {printable}')
+    if show:
+        for line in results:
+            print(line)
+        else:
+            return results
+def receive_from(connection):
+    buffer=b''
+    connection.settimeout(10)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer+= data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    #enter packet modifications
+    return buffer
+
+def response_handler(buffer):
+    #perform packet modifications
+    return buffer
+
+def proxy_handler(client_socket,remote_host,remote_port,receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host,remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+    remote_buffer = response_handler(remote_buffer)
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhost."% len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>]Received %d bytes from localhost."%len(remote_buffer)
+            print(line)
+            hexdump(local_buffer)
+
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==>] Sent to remote.")
+
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[==>] Received %d from remote." % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[==>] Sent to localhost.")
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data. Closing all connections.")
+            break
+        
+
+hexdump('python rocks\n and proxies roll\n')
